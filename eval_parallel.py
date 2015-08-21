4822eacbbbe1941ae0433d7fda975b1186c74cd7
cbuil <cbuil@eb33f05e-5d3b-421d-9d62-fdc748bd3602>
2015-08-21 16:28:12 +0000

    modified parallel python script
    git-svn-id: http://www.svn.ciws.cl/tpf_evaluation/src/Client.js@53 eb33f05e-5d3b-421d-9d62-fdc748bd3602

--- eval_parallel.py
+++ eval_parallel.py
@@ -4,48 +4,18 @@ import sys
 import os
 import glob
 import subprocess
-import shlex
-import threading
 
 
 AVAILABLE_CORES = 10
-
-# _TPF_CLIENT_COMMAND = '''results=$(timeout %s %s %s %s
-#     | tee >(grep 'Request\s#' | wc -l
-#         | awk  '{ print "Requests: "$1}') >(grep 'Result #' | wc -l
-#         | awk  '{ print "Results: "$1}') >(grep 'First Result Time'
-#         | awk  '{ print "Time: "$4}' ) )'''
-
-
-# class Command(object):
-#     def __init__(self, cmd):
-#         self.cmd = cmd
-#         self.process = None
-
-#     def run(self, timeout):
-#         def target():
-#             print 'Thread started'
-#             self.process = subprocess.Popen(self.cmd, shell=True)
-#             self.process.communicate()
-#             print 'Thread finished'
-
-#         thread = threading.Thread(target=target)
-#         thread.start()
-
-#         thread.join(timeout)
-#         if thread.is_alive():
-#             print 'Terminating process'
-#             self.process.terminate()
-#             thread.join()
-#         print self.process.returncode
 
 
 def eval_parallel(command, server, query_folder):
     arglist = []
     total_counter = 0
-    for query_file in sorted(glob.glob(query_folder + '/*')):
-        print(command, server, query_file)
-        arglist.append((command, server, query_file))
+    for query_dir in sorted(glob.glob(query_folder + '/*')):
+        for query_file in sorted(glob.glob(query_folder + '/*')):
+            print(command, server, query_file)
+            arglist.append((command, server, query_file))
     print('Processing ' + str(total_counter) + ' queries ' +
           str(AVAILABLE_CORES) + ' cores, this may take a while...')
     pool = Pool(processes=AVAILABLE_CORES)
@@ -79,7 +49,6 @@ def main((command, config_file, query_folder)):
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        print('Usage: python eval_parallel.py config_file query_folder')
+        print('Usage: python TPF_command config_file query_folder')
     else:
-        # (command, server, query_folder)
         eval_parallel(sys.argv[1], sys.argv[2], sys.argv[3])
