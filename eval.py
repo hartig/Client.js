b03aff3b8cdfe9d97076ba7c625fca6f71f858f7
cbuil <cbuil@eb33f05e-5d3b-421d-9d62-fdc748bd3602>
2016-03-29 20:21:16 +0000

    modified eval and deleted old config.json
    git-svn-id: http://www.svn.ciws.cl/tpf_evaluation/src/Client.js@81 eb33f05e-5d3b-421d-9d62-fdc748bd3602

--- eval.py
+++ eval.py
@@ -3,12 +3,18 @@ import sys
 import os
 import glob
 import subprocess
+import signal
 
 
-def main(command, start_fragment, config_file, query_folder):
+def handler(signum, frame):
+    print "Forever is over!"
+    raise Exception("end of time")
+
+
+def main(command, start_fragment, config_file, query_folder, batch):
     for query_file in sorted(glob.glob(query_folder + '/*.rq')):
         print('Query: ' + query_file)
-        cmd = './bin/' + command + ' ' + start_fragment + ' -c ' + config_file + ' -f ' + os.path.join(os.path.dirname(os.path.realpath(__file__)), query_file)
+        cmd = './bin/' + command + ' ' + start_fragment + ' -c ' + config_file + ' -f ' + os.path.join(os.path.dirname(os.path.realpath(__file__)), query_file + ' --maxNumberOfMappings ' + batch)
         print('Command: ' + cmd)
         try:
             subprocess.call(cmd, shell=True)
@@ -22,5 +28,12 @@ if __name__ == '__main__':
         print('Usage: python eval.py config_file query_folder')
     else:
         # (command, server, query_folder)
-        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        signal.signal(signal.SIGALRM, handler)
+        signal.alarm(60)
+        try:
+            main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+        except Exception, exc:
+            print exc
+            sys.exit()
+	sys.exit()
 
