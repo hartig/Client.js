d86b7e9e471f223efab2a9924384682a257848ca
cbuil <cbuil@eb33f05e-5d3b-421d-9d62-fdc748bd3602>
2016-03-30 17:19:45 +0000

    added script to run clients in parallel
    git-svn-id: http://www.svn.ciws.cl/tpf_evaluation/src/Client.js@86 eb33f05e-5d3b-421d-9d62-fdc748bd3602

---
+++ eval_parallel.py
@@ -0,0 +1,54 @@
+import traceback
+import sys
+import os
+import glob
+import subprocess
+import signal
+from multiprocessing import Pool
+
+
+def handler(signum, frame):
+    print "Forever is over!"
+    raise Exception("end of time")
+
+
+def main_parallel(command, start_fragment, config_file, query_folders, batch):
+    arglist = []
+    AVAILABLE_CORES = 22
+    cnt = 0
+    for query_folder in sorted(glob.glob(query_folders)):
+        arglist.append(command, start_fragment, config_file, query_folder, batch)
+        cnt += 1
+
+    print('Processing ' + str(cnt) + ' files with ' +
+          str(AVAILABLE_CORES) + ' cores, this may take a while...')
+    pool = Pool(processes=AVAILABLE_CORES)
+    pool.map_async(main, arglist).get()
+
+
+def main((command, start_fragment, config_file, query_folder, batch)):
+    for query_file in sorted(glob.glob(query_folder + '/*.rq')):
+        print('Query: ' + query_file)
+        cmd = './bin/' + command + ' ' + start_fragment + ' -c ' + config_file + ' -f ' + os.path.join(os.path.dirname(os.path.realpath(__file__)), query_file + ' --maxNumberOfMappings ' + batch)
+        print('Command: ' + cmd)
+        try:
+            subprocess.call(cmd, shell=True)
+        except Exception, e:
+            print(e)
+            traceback.print_exc(file=sys.stdout)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print('Usage: python eval.py config_file query_folder')
+    else:
+        # (command, server, query_folder)
+        signal.signal(signal.SIGALRM, handler)
+        signal.alarm(360)
+        try:
+            main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+        except Exception, exc:
+            print exc
+            sys.exit()
+        sys.exit()
+
