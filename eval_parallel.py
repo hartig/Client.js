5aef6406791d0325179a8b80cc11482553b15e85
cbuil <cbuil@eb33f05e-5d3b-421d-9d62-fdc748bd3602>
2016-04-21 15:00:00 +0000

    found a bug writing combined with timeout
    git-svn-id: http://www.svn.ciws.cl/tpf_evaluation/src/Client.js@139 eb33f05e-5d3b-421d-9d62-fdc748bd3602

--- eval_parallel.py
+++ eval_parallel.py
@@ -22,18 +22,20 @@ def main_parallel(command, start_fragment, config_file, query_folders, batch, co
 
 
 def main((command, start_fragment, config_file, query_folder, batch, folder_number)):
-    with open('executed_queries_list_' + str(folder_number) + '.txt', 'w') as query_list_file:
         for query_file in sorted(glob.glob(query_folder + '/*.rq')):
-            query_list_file.write(query_file + '\n')
-            cmd = 'bin/' + command + ' ' + start_fragment + ' -c ' + config_file + ' -f ' + os.path.join(os.path.dirname(os.path.realpath(__file__)), query_file + ' --maxNumberOfMappings ' + batch  + ' --outputFileNumber ' + str(folder_number))
-            print('Command: ' + cmd)
-            try:
-                subprocess.call(cmd, shell=True)
-            except Exception, e:
-                print(e)
-                query_list_file.write(e + '\n')
-                query_list_file.write('error in query: ' + query_file + '\n')
-                traceback.print_exc(file=sys.stdout)
+            print('Query: ' + query_file)
+            query_list_file_name = 'executed_queries_list_' + str(folder_number) + '.txt'
+            with open(query_list_file_name, 'a') as query_list_file:
+                query_list_file.write(query_file + '\n')
+                cmd = 'bin/' + command + ' ' + start_fragment + ' -c ' + config_file + ' -f ' + os.path.join(os.path.dirname(os.path.realpath(__file__)), query_file + ' --maxNumberOfMappings ' + batch  + ' --outputFileNumber ' + str(folder_number))
+                print('Command: ' + cmd)
+                try:
+                    subprocess.call(cmd, shell=True)
+                except Exception, e:
+                    print(e)
+                    query_list_file.write(e + '\n')
+                    query_list_file.write('error in query: ' + query_file + '\n')
+                    traceback.print_exc(file=sys.stdout)
 
 
 if __name__ == '__main__':
