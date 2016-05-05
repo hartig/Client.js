2d9a5b67ee0d8e69ddadca743f7e3c6675897fcc
cbuil <cbuil@eb33f05e-5d3b-421d-9d62-fdc748bd3602>
2016-05-05 12:18:39 +0000

    fixed dir creation in script
    git-svn-id: http://www.svn.ciws.cl/tpf_evaluation/src/Client.js@235 eb33f05e-5d3b-421d-9d62-fdc748bd3602

--- eval.py
+++ eval.py
@@ -4,7 +4,7 @@ import os
 import glob
 import subprocess
 import signal
-
+from datetime import datetime
 
 class Timeout():
     """Timeout class using ALARM signal"""
@@ -28,7 +28,8 @@ class Timeout():
 def main(command, start_fragment, config_file, query_folder, batch):
     for query_file in sorted(glob.glob(query_folder + '/*.rq')):
         print('Query: ' + query_file)
-        cmd = './bin/' + command + ' ' + start_fragment + ' -c ' + config_file + ' -f ' + os.path.join(os.path.dirname(os.path.realpath(__file__)), query_file + ' --maxNumberOfMappings ' + batch)
+        cmd = 'bin/' + command + ' ' + start_fragment + ' -c ' + config_file + ' -f ' + os.path.join(os.path.dirname(os.path.realpath(__file__)), query_file + ' --maxNumberOfMappings ' + batch  + ' --outputFileNumber ' + str(0))
+        #cmd = './bin/' + command + ' ' + start_fragment + ' -c ' + config_file + ' -f ' + os.path.join(os.path.dirname(os.path.realpath(__file__)), query_file + ' --maxNumberOfMappings ' + batch)
         print('Command: ' + cmd)
         try:
             subprocess.call(cmd, shell=True)
@@ -42,10 +43,9 @@ if __name__ == '__main__':
         print('Usage: python eval.py config_file query_folder')
     else:
         # (command, server, query_folder)
-        try:
-            with Timeout(3):
-                main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
-        except Timeout.Timeout:
-            print "Timeout"
-            sys.exit()
-        sys.exit()
+        startTime = datetime.now()
+        print("starting time: " + str(startTime))
+        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+        print("end time: " + str(datetime.now()))
+        print("Total time: " + str(datetime.now() - startTime))
+        print datetime.now() - startTime
